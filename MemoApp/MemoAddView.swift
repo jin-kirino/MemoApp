51abbf17e9ebb34e201fe9b6e1c85257981eb1c3
jin-kirino <jinkirino@gmail.com>
2022-07-23 17:46:39 +0900

    メモ追加画面を作成

---
+++ MemoApp/MemoAddView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoAddView.swift
+//  MemoApp
+//
+//  Created by 神　樹里乃 on 2022/07/23.
+//
+
+import SwiftUI
+
+struct MemoAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoAddView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoAddView()
+    }
+}
