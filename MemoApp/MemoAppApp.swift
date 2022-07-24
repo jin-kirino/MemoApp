ce0b7ca3ce2df303b05b2e2c1a5df2f6532eb84b
jin-kirino <jinkirino@gmail.com>
2022-07-24 17:29:32 +0900

    コメントを追加

--- MemoApp/MemoAppApp.swift
+++ MemoApp/MemoAppApp.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 @main
 struct MemoAppApp: App {
+    // NSPersistentContainer（各クラスをラッピングして使えるようにするクラス）の初期化
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // 環境変数 @Environment(\.managedObjectContext)にNSManagedObjectContextを登録する処理
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
