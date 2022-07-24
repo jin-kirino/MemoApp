c0015987f72249fb60797d4bb11f48bced89cc05
jin-kirino <jinkirino@gmail.com>
2022-07-24 17:27:50 +0900

    NavigationbarTitleを設定

--- MemoApp/ContentView.swift
+++ MemoApp/ContentView.swift
@@ -9,77 +9,56 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var memo: String = ""
+    @State private var memos: [String] = []
+
+    init() {
+        // UINavigationBarAppearanceを使ってnavigationTitleをカスタマイズ
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        // 背景に黒を指定
+        appearance.backgroundColor = .white
+        // 文字色に白を指定
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+
+    // 被管理オブジェクトコンテキスト(ManagedObjectContext)の取得
+    // データベースから引っ張ってきたコードデータ、必ずMOCと紐づいている
+//    @Environment(\.managedObjectContext) private var viewContext
+    // データベースよりデータを取得
+//    @FetchRequest(
+        // key:日付、ascending: true→昇順（新しいのが下）で並べ替える
+//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+//        animation: .default)
+//    private var items: FetchedResults<Item>
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
+            ZStack {
+                Color.gray
+                    .ignoresSafeArea()
+                VStack {
+                    List {
+                        ForEach(memos, id: \.self) { memo in
+                            Text("\(memo)")
+                        }// ForEach
+                        .onDelete(perform: removeRows)
+                    }// List
+                }// VStack
+            }// ZStack
+            .navigationBarTitle("メモの一覧")
+        }// NavigationView
+    }// body
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+    // 行を削除する関数
+    func removeRows(at offsets: IndexSet) {
+        memos.remove(atOffsets: offsets)
+    }// removeRows
+}// ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
