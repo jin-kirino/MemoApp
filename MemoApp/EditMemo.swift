82cbb61eeda1cc68bf467109a537fb513b4df143
jin-kirino <jinkirino@gmail.com>
2022-07-30 14:22:35 +0900

    new file

---
+++ MemoApp/EditMemo.swift
@@ -0,0 +1,20 @@
+//
+//  EditMemo.swift
+//  MemoApp
+//
+//  Created by 神　樹里乃 on 2022/07/30.
+//
+
+import SwiftUI
+
+struct EditMemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditMemo_Previews: PreviewProvider {
+    static var previews: some View {
+        EditMemo()
+    }
+}
