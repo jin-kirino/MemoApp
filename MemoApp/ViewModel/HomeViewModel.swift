4d48754746f67455e963070133e0c35141691245
jin-kirino <jinkirino@gmail.com>
2022-08-21 16:55:59 +0900

    HomeViewModelを追加

---
+++ MemoApp/ViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  MemoApp
+//
+//  Created by 神　樹里乃 on 2022/08/21.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
