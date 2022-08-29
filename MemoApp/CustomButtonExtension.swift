693ef0ea924b0adaaa17be847c31232d05f82659
jin-kirino <jinkirino@gmail.com>
2022-08-30 08:40:27 +0900

    ファイル名を変更

--- MemoApp/CustomButtonExtension.swift
+++ MemoApp/CustomButtonExtension.swift
@@ -1,5 +1,5 @@
 //
-//  ButtonColor.swift
+//  CustomButton.swift
 //  MemoApp
 //
 //  Created by 神　樹里乃 on 2022/08/28.
@@ -9,6 +9,6 @@ import Foundation
 import SwiftUI
 
 extension Color {
-    static let startColor = Color("startColor")
-    static let finishColor = Color("finishColor")
+    static let customBlue = Color("customBlue")
+    static let customGreen = Color("customGreen")
 }
